b3717845abc53973baf06d750d1cd03fde63e53d
Juan Rodriguez Garrido <jrodgar@aulaestudio.es>
2026-01-28 19:33:05 +0100

    Reestructura del main simple

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,12 @@ public class Main {
     public static void main(String[] args) {
       
         //Creamos el mazo:
-         Mazo mazo = new Mazo();
-         //Lo rellenamos:
-         mazo.rellenarMazo();
+        Mazo mazo = new Mazo();
+        //Como vemos aquí está ordenado:
          System.out.println(mazo);
-
+        //Lo barajamos:
         mazo.barajar();
+        //Comprobamos:
         System.out.println(mazo);
 
     }
