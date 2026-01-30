66bec94db23b6d07a99cb4d8d90ce0e21d0ca7cf
Juan Rodriguez Garrido <jrodgar@aulaestudio.es>
2026-01-30 16:51:29 +0100

    Finalizados todos los métodos, menú creado

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,59 @@
 package Baraja;
 
-public class Main {
-    
-    public static void main(String[] args) {
-      
-        //Creamos el mazo:
-        Mazo mazo = new Mazo();
-        //Como vemos aquí está ordenado:
-         System.out.println(mazo);
-        //Lo barajamos:
-        mazo.barajar();
-        //Comprobamos:
-        System.out.println(mazo);
+import java.util.Scanner;
 
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Creamos el mazo:
+        Mazo mazo = new Mazo();
+        Scanner sn = new Scanner(System.in);
+        boolean checker = false;
+
+        while (!checker) {
+            System.out.println("""
+                    ****** MENÚ PRINCIPAL ******
+                    1. Barajar\s
+                    2. Siguiente carta\s
+                    3. Ver cuántas cartas disponibles hay\s
+                    4. Dar cartas por cantidad\s
+                    5. Ver que cartas hay en el montón\s
+                    6. Ver que cartas hay en el mazo\s
+                    7. Salir
+                    ***********************""");
+            int eleccion = sn.nextInt();
+            switch (eleccion) {
+                case 1:
+                    mazo.barajar();
+                    break;
+                case 2:
+                    System.out.println(mazo.siguienteCarta());
+                    break;
+                case 3:
+                    System.out.println(mazo.cartasDisponibles());
+                    break;
+                case 4:
+                    System.out.println("Cuantas cartas quieres dar?");
+                    int n = sn.nextInt();
+                    mazo.darCartas(n);
+                    break;
+                case 5:
+                    mazo.cartasMonton();
+                    break;
+                case 6:
+                    mazo.mostrarBaraja();
+                    break;
+                case 7:
+                    checker = true;
+                    System.out.println("******** SALIENDO ********");
+                    break;
+                default:
+                    System.out.println("Elige una opción de la lista");
+                    break;
+
+            }
+        }
     }
+
 }
--- src/Mazo.java
+++ src/Mazo.java
@@ -4,14 +4,12 @@ import java.util.Random;
 
 public class Mazo {
     private Carta mazo[];
-    private Carta cartasSalidas[];
     private int contadorSalidas;
-    private static String[] opcionesPalo = {"Picas", "Oros", "Diamantes", "Bastos"};
+    private static String[] opcionesPalo = { "Picas", "Oros", "Diamantes", "Bastos" };
     private static Random rnd = new Random();
 
     public Mazo() {
-        this.mazo = new Carta[40]; //Creamos un array de tipo Carta con 40 huecos para cartas
-        this.cartasSalidas = new Carta[40];
+        this.mazo = new Carta[40];
         rellenarMazo();
     }
 
@@ -20,7 +18,7 @@ public class Mazo {
         for (String p : opcionesPalo) {
             for (int z = 1; z < 11; z++) {
                 if (z >= 8) {
-                    mazo[i] = new Carta((z + 2), p); //Aquí llenamos el mazo de cartas
+                    mazo[i] = new Carta((z + 2), p); // Aquí llenamos el mazo de cartas
                 } else {
                     mazo[i] = new Carta(z, p);
                 }
@@ -29,30 +27,66 @@ public class Mazo {
         }
     }
 
-
-    public void barajar(){
-    Carta auxiliar; 
-    int random = 0;
-        for(int i = 0; i < mazo.length; i++){
+    public void barajar() {
+        Carta auxiliar; // aqui guardaremos la carta que cambiaremos
+        int random = 0;
+        for (int i = 0; i < mazo.length; i++) {
             random = rnd.nextInt(40);
             auxiliar = mazo[random];
             mazo[random] = mazo[i];
             mazo[i] = auxiliar;
         }
+        System.out.println("El mazo ha sido barajado \n");
+    }
+
+    public String siguienteCarta() {
+        if (contadorSalidas >= mazo.length) {
+            return "No quedan cartas";
+        }
+
+        Carta siguienteCarta = mazo[contadorSalidas]; // Guardamos la carta
+
+        this.contadorSalidas++;
+        return siguienteCarta.toString();
 
     }
 
-    public Carta siguienteCarta(){
-        if(contadorSalidas >= mazo.length){
-            System.out.println("No quedan cartas");
-            return null;
+    public String cartasDisponibles() {
+        return "Quedan " + (this.mazo.length - this.contadorSalidas) + " cartas disponibles";
+
+    }
+
+    public void darCartas(int cantidad) {
+        if (this.mazo.length - this.contadorSalidas >= cantidad) {
+            for (int i = 0; i < cantidad; i++) {
+                System.out.println(mazo[this.contadorSalidas]);
+                this.contadorSalidas++;
+            }
+        } else {
+            System.out.println("No quedan suficientes cartas");
         }
+    }
 
-        Carta siguienteCarta = mazo[contadorSalidas]; //Guardamos la carta
-        this.cartasSalidas[contadorSalidas] = siguienteCarta;
-        this.contadorSalidas++;
-        return siguienteCarta;
+    public void cartasMonton() {
+        if (contadorSalidas > 0) {
+            System.out.println("Cartas en el montón: ");
+            for (int i = 0; i < contadorSalidas; i++) {
+                System.out.println(mazo[i]);
+            }
+        } else {
+            System.out.println("No ha salido ninguna carta!");
+        }
+    }
 
+    public void mostrarBaraja() {
+        if (contadorSalidas < this.mazo.length) {
+            System.out.println("Cartas aún en el mazo:");
+            for (int i = contadorSalidas; i < this.mazo.length; i++) {
+                System.out.println(mazo[i]);
+            }
+        } else {
+            System.out.println("No quedan cartas en el mazo");
+        }
     }
 
     @Override
@@ -65,18 +99,13 @@ public class Mazo {
         return sb.toString();
     }
 
-
-
-
-
-
-
-
-    /* METODO PARA CREAR UNA SOLA CARTA AL AZAR (NO UTIL EN ESTE CASO)
-    public static Carta generarCarta(){
-        int num = rnd.nextInt((12  ) + 1);
-        String palo = opcionesPalo[rnd.nextInt(opcionesPalo.length)];
-
-        return new Carta(num, palo);
-    } */
+    /*
+     * METODO PARA CREAR UNA SOLA CARTA AL AZAR (NO UTIL EN ESTE CASO)
+     * public static Carta generarCarta(){
+     * int num = rnd.nextInt((12 ) + 1);
+     * String palo = opcionesPalo[rnd.nextInt(opcionesPalo.length)];
+     * 
+     * return new Carta(num, palo);
+     * }
+     */
 }
